1cb524d17eba703daf8b3c13314d7c87e3c5cf04
watataku8911 <watato.takuya@gmail.com>
2021-12-14 15:37:51 +0900

    third-commit

--- src/components/Chip.vue
+++ src/components/Chip.vue
@@ -19,6 +19,18 @@ export default {
 .chip {
   position: relative;
   top: -1.5vh;
+  height: 32px;
+  font-size: 1em;
+  font-weight: 500;
+  color: rgba(0, 0, 0, 0.6);
+  line-height: 32px;
+  padding: 0 15px;
+  border-radius: 16px;
+  background-color: #e4e4e4;
+  margin-bottom: 5px;
+  margin-right: 5px;
+  display: inline-block;
+  text-align: center;
 }
 
 .chip::before {
